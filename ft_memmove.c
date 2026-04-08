6189948b20774c3e4b76dcb6fc4e1f9ff494ae9e
pmalato <pedrocoelhomalato@gmail.com>
2026-04-08 11:05:56 +0100

    Add files via upload

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pecoelho <pecoelho@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: pecoelho <pecoelho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 09:57:22 by pecoelho          #+#    #+#             */
-/*   Updated: 2026/04/05 07:48:52 by pecoelho         ###   ########.fr       */
+/*   Updated: 2026/04/08 10:59:18 by pecoelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,19 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char		*temp;
 	const unsigned char	*s;
 	unsigned char		*d;
-	size_t				i;
 
+	if (!src || !dest)
+		return (NULL);
 	s = (const unsigned char *)src;
 	d = (unsigned char *)dest;
-	temp = malloc(n);
-	if (!temp)
-		return (NULL);
-	i = 0;
-	while (i < n)
+	if (d > s)
 	{
-		temp[i] = s[i];
-		i++;
+		while (n--)
+			d[n] = s[n];
 	}
-	i = 0;
-	while (i < n)
-	{
-		d[i] = temp[i];
-		i++;
-	}
-	free(temp);
+	else
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
